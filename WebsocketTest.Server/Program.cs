16273d03ad0ebe6f6a83ca334b5816df5a2011f8
ice02 <iceman02@live.fr>
2019-04-06 18:21:03 +0200

    bug resolution

--- WebsocketTest.Server/Program.cs
+++ WebsocketTest.Server/Program.cs
@@ -5,72 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using WebSocketSharp;
 using WebSocketSharp.Server;
-//using SuperSocket;
-//using SuperSocket.ProtoBase;
-//using SuperSocket.Server;
 
 namespace WebsocketTest.Server
 {
     class Program
     {
-        //static SuperSocketServer CreateSocketServer<TPackageInfo, TPipelineFilter>(Dictionary<string, string> configDict = null, Func<IAppSession, TPackageInfo, Task> packageHandler = null)
-        //    where TPackageInfo : class
-        //    where TPipelineFilter : IPipelineFilter<TPackageInfo>, new()
-
-        //{
-
-        //    if (configDict == null)
-
-        //    {
-
-        //        configDict = new Dictionary<string, string>
-
-        //        {
-
-        //            { "serverOptions:name", "TestServer" },
-
-        //            { "serverOptions:listeners:0:ip", "Any" },
-
-        //            { "serverOptions:listeners:0:port", "4040" }
-
-        //        };
-
-        //    }
-
-
-
-        //    var server = new SuperSocketServer();
-
-
-
-        //    var services = new ServiceCollection();
-
-
-
-        //    var builder = new ConfigurationBuilder().AddInMemoryCollection(configDict);
-
-        //    var config = builder.Build();
-
-
-
-        //    services.AddLogging(loggingBuilder => loggingBuilder.AddConsole());
-
-
-
-        //    var serverOptions = new ServerOptions();
-
-        //    config.GetSection("serverOptions").Bind(serverOptions);
-
-
-
-        //    server.Configure<TPackageInfo, TPipelineFilter>(serverOptions, services, packageHandler: packageHandler);
-
-
-
-        //    return server;
-
-        //}
-
         public class Toolbox : WebSocketBehavior
         {
             protected override void OnMessage(MessageEventArgs e)
@@ -87,6 +26,13 @@ namespace WebsocketTest.Server
                 base.OnOpen();
 
                 Console.WriteLine("Opening");
+            }
+
+            protected override void OnError(ErrorEventArgs e)
+            {
+                base.OnError(e);
+
+                Console.WriteLine($"Error : {e.Message}");
             }
         }
 
@@ -107,6 +53,8 @@ namespace WebsocketTest.Server
             //    return true; // If the server certificate is valid.
             //};
 
+            wssv.Log.Level = LogLevel.Trace;
+
             wssv.Start();
             Console.ReadKey(true);
             wssv.Stop();
@@ -115,45 +63,6 @@ namespace WebsocketTest.Server
 
 
         }
-
-
-
-        //static async Task RunAsync()
-
-        //{
-
-        //    var server = CreateSocketServer<TextPackageInfo, LinePipelineFilter>(packageHandler: async (s, p) =>
-
-        //    {
-
-        //        await s.Channel.SendAsync(Encoding.UTF8.GetBytes(p.Text).AsMemory());
-
-        //    });
-
-
-
-        //    await server.StartAsync();
-
-
-
-        //    Console.WriteLine("The server is started.");
-
-
-
-        //    while (Console.ReadLine().ToLower() != "c")
-
-        //    {
-
-        //        continue;
-
-        //    }
-
-
-
-        //    await server.StopAsync();
-
-        //}
-
     }
 }
 
--- WebsocketTest/ClientApp/src/components/ProductDetail.js
+++ WebsocketTest/ClientApp/src/components/ProductDetail.js
@@ -4,14 +4,14 @@ import Websocket from 'react-websocket';
 export class ProductDetail extends React.Component {
     displayName = ProductDetail.name
 
-    refWebSocket = new WebSocket('ws://localhost:8888/Toolbox')
-
     constructor(props) {
         super(props);
         this.state = {
             count: 90
         };
     }
+
+    refWebSocket = new WebSocket('ws://localhost:8888/Toolbox')
 
     handleData(data) {
         alert(data);
