92885eebd507d66f5da1a588894e29e18f127898
ice02 <iceman02@live.fr>
2019-04-06 12:34:24 +0200

    client server v1

---
+++ WebsocketTest.Server/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+//using SuperSocket;
+//using SuperSocket.ProtoBase;
+//using SuperSocket.Server;
+
+namespace WebsocketTest.Server
+{
+    class Program
+    {
+        //static SuperSocketServer CreateSocketServer<TPackageInfo, TPipelineFilter>(Dictionary<string, string> configDict = null, Func<IAppSession, TPackageInfo, Task> packageHandler = null)
+        //    where TPackageInfo : class
+        //    where TPipelineFilter : IPipelineFilter<TPackageInfo>, new()
+
+        //{
+
+        //    if (configDict == null)
+
+        //    {
+
+        //        configDict = new Dictionary<string, string>
+
+        //        {
+
+        //            { "serverOptions:name", "TestServer" },
+
+        //            { "serverOptions:listeners:0:ip", "Any" },
+
+        //            { "serverOptions:listeners:0:port", "4040" }
+
+        //        };
+
+        //    }
+
+
+
+        //    var server = new SuperSocketServer();
+
+
+
+        //    var services = new ServiceCollection();
+
+
+
+        //    var builder = new ConfigurationBuilder().AddInMemoryCollection(configDict);
+
+        //    var config = builder.Build();
+
+
+
+        //    services.AddLogging(loggingBuilder => loggingBuilder.AddConsole());
+
+
+
+        //    var serverOptions = new ServerOptions();
+
+        //    config.GetSection("serverOptions").Bind(serverOptions);
+
+
+
+        //    server.Configure<TPackageInfo, TPipelineFilter>(serverOptions, services, packageHandler: packageHandler);
+
+
+
+        //    return server;
+
+        //}
+
+        public class Toolbox : WebSocketBehavior
+        {
+            protected override void OnMessage(MessageEventArgs e)
+            {
+                var msg = e.Data == "Version"
+                          ? "1.0.0"
+                          : "0";
+
+                Send(msg);
+            }
+
+            protected override void OnOpen()
+            {
+                base.OnOpen();
+
+                Console.WriteLine("Opening");
+            }
+        }
+
+
+        static void Main(string[] args)
+
+        {
+
+            //var wssv = new WebSocketServer(5055, true);
+            var wssv = new WebSocketServer("ws://localhost:8888");
+            wssv.AddWebSocketService<Toolbox>("/Toolbox");
+
+            //wssv.SslConfiguration.ServerCertificateValidationCallback =
+            //    (sender, certificate, chain, sslPolicyErrors) => {
+            //    // Do something to validate the server certificate.
+            //    ...
+
+            //    return true; // If the server certificate is valid.
+            //};
+
+            wssv.Start();
+            Console.ReadKey(true);
+            wssv.Stop();
+
+            //RunAsync().Wait();
+
+
+        }
+
+
+
+        //static async Task RunAsync()
+
+        //{
+
+        //    var server = CreateSocketServer<TextPackageInfo, LinePipelineFilter>(packageHandler: async (s, p) =>
+
+        //    {
+
+        //        await s.Channel.SendAsync(Encoding.UTF8.GetBytes(p.Text).AsMemory());
+
+        //    });
+
+
+
+        //    await server.StartAsync();
+
+
+
+        //    Console.WriteLine("The server is started.");
+
+
+
+        //    while (Console.ReadLine().ToLower() != "c")
+
+        //    {
+
+        //        continue;
+
+        //    }
+
+
+
+        //    await server.StopAsync();
+
+        //}
+
+    }
+}
+
---
+++ WebsocketTest/ClientApp/src/components/ProductDetail.js
@@ -0,0 +1,89 @@
+﻿import React from 'react';
+import Websocket from 'react-websocket';
+
+export class ProductDetail extends React.Component {
+    displayName = ProductDetail.name
+
+    refWebSocket = new WebSocket('ws://localhost:8888/Toolbox')
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 90
+        };
+    }
+
+    handleData(data) {
+        alert(data);
+        let result = JSON.parse(data);
+        this.setState({ count: this.state.count + result.movement });
+    }
+
+    sendMessage(message) {
+        this.refWebSocket.sendMessage(message);
+    }
+
+    render() {
+        return (
+            <div>
+                Count: <strong>{this.state.count} </strong>
+                <button onClick={() => this.sendMessage("Version")} >Send Message</button>
+                <Websocket url='ws://localhost:8888/Toolbox'
+                    onMessage={this.handleData.bind(this)}
+                    ref={Websocket => {
+                        this.refWebSocket = Websocket;
+                    }}/>
+            </div>
+        );
+    }
+}
+
+
+
+//import React from 'react';
+//import Websocket from 'react-websocket';
+
+//export class ProductDetail extends React.Component {
+//    displayName = ProductDetail.name
+
+//    refWebSocket = new WebSocket('ws://localhost:8888/Toolbox')
+
+
+//    constructor(props) {
+//        super(props);
+//        this.state = {
+//            count: 90
+//        };
+//    }
+
+//    handleData(data) {
+//        let result = JSON.parse(data);
+//        this.setState({ count: this.state.count + result.movement });
+//    }
+
+//    sendMessage(message) {
+//        this.refWebSocket.sendMessage(message);
+//    }
+
+//    render() {
+//        return (
+//            <div>
+//                Count: <strong>{this.state.count} </strong>
+
+
+//                <button onClick={() => this.sendMessage("Version")} >Send Message</button>
+//                <Websocket
+//                    url=''
+//                    onMessage={this.handleData.bind(this)}
+//                    onOpen={this.handleOpen} onClose={this.handleClose}
+//                    reconnect={true} debug={true}
+
+//                    ref={Websocket => {
+//                        this.refWebSocket = Websocket;
+//                    }}
+
+//                />
+//            </div>
+//        );
+//    }
+//}
